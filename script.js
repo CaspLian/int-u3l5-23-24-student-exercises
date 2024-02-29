15ff39457d092b405adcc6ec51bf5bf2ff19d535
CaspLian <149088248+CaspLian@users.noreply.github.com>
2024-02-29 16:00:40 +0000

    2/29

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ let button = document.querySelector("button");
 let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
+//it changed the background color and the header color
 // - What do you think .style does?
+//I think it changes CSS
 
 // 2. Try the following:
 // - Make it so that the colors are different than what's used.
 // - Try using a hex color code.
 button.addEventListener("click", function() {
-    body.style.backgroundColor = "black";
-    header.style.color = "white";
-    img.style.border = "5px solid white";
+    body.style.backgroundColor = "green";
+    header.style.color = "orange";
+    img.style.border = "5px solid blue";
 });
